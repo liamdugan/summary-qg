70ca752131d4f90ba84ae80bb3a92521f69435ec
liamdugan <ldugan@seas.upenn.edu>
2022-03-13 15:26:29 -0400

    Added coverage analysis code and other optimizations

---
+++ coverage.py
@@ -0,0 +1,36 @@
+import pandas, argparse
+
+parser = argparse.ArgumentParser(description='Do coverage analysis for keywords')
+parser.add_argument('keyword_file', help='A file containing keywords', type=str)
+parser.add_argument('data_file', help='A file containing questions and answers', type=str)
+args = parser.parse_args()
+
+keyword_df = pandas.read_csv(args.keyword_file)
+keywords = keyword_df.words.unique()
+data = pandas.read_csv(args.data_file)
+
+'''
+  Computes the percentage of keywords that were present as exact substrings in a given set of question-answer pairs.
+  Output is a tuple of the form (% in questions, % in answers, % in both Qs and As)
+'''
+def percent_keywords_used_in_questions(input_df):
+    def exact_match(series):
+        return lambda keyword: series.apply(lambda q: keyword in q).any()
+
+    def compute_coverage(keywords, data):
+        fq = exact_match(data['Question'])
+        fa = exact_match(data['Answer'])
+
+        q_mask = keywords['words'].apply(fq)
+        a_mask = keywords['words'].apply(fa)
+        qa_mask = q_mask | a_mask
+
+        return tuple(len(keywords[mask]) / len(keywords) for mask in [q_mask, a_mask, qa_mask])
+
+    return compute_coverage(keyword_df, input_df)
+
+
+for author in data.Author.unique():
+    print(author, percent_keywords_used_in_questions(data[data['Author'] == author]))
+    
+print("Human Summary (Total)", percent_keywords_used_in_questions(data[data['Author'].str.contains("Human Summary")]))
--- run_qg.py
+++ run_qg.py
@@ -18,29 +18,29 @@ parser.add_argument('-i','--infile', help="The name of the text file to generate
 args = parser.parse_args()
 
 def print_qa_pairs(qa_pairs):
-  for pair in qa_pairs:
-    print(pair)
+    for pair in qa_pairs:
+        print(pair)
 
 if __name__ == "__main__":
 
-  qg_model = "valhalla/t5-small-qa-qg-hl" if args.fast else "valhalla/t5-base-qa-qg-hl"
-  sum_model = "sshleifer/distilbart-cnn-6-6" if args.fast else "facebook/bart-large-cnn"
-  
-  qg = pipeline("multitask-qa-qg", model=qg_model)
-  tokenizer = AutoTokenizer.from_pretrained("t5-base")
-  
-  if torch.cuda.is_available():
-    summarizer = pipelineHF("summarization", model=sum_model, device=0) if args.use_summary else None
-  else:
-    summarizer = pipelineHF("summarization", model=sum_model) if args.use_summary else None
+    qg_model = "valhalla/t5-small-qa-qg-hl" if args.fast else "valhalla/t5-base-qa-qg-hl"
+    sum_model = "sshleifer/distilbart-cnn-6-6" if args.fast else "facebook/bart-large-cnn"
 
-  if args.infile:
-    input_text = open(args.infile,"r").read()
-    qa_pairs = extract_qa_pairs(tokenizer, qg, summarizer, input_text)
-    print_qa_pairs(qa_pairs)
-  
-  else:
-    while True:
-      input_text = input(">")
-      qa_pairs = extract_qa_pairs(tokenizer, qg, summarizer, input_text)
-      print_qa_pairs(qa_pairs)
+    qg = pipeline("multitask-qa-qg", model=qg_model)
+    tokenizer = AutoTokenizer.from_pretrained("t5-base")
+
+    if torch.cuda.is_available():
+        summarizer = pipelineHF("summarization", model=sum_model, device=0) if args.use_summary else None
+    else:
+        summarizer = pipelineHF("summarization", model=sum_model) if args.use_summary else None
+
+    if args.infile:
+        input_text = open(args.infile,"r").read()
+        qa_pairs = extract_qa_pairs(tokenizer, qg, summarizer, input_text)
+        print_qa_pairs(qa_pairs)
+
+    else:
+        while True:
+            input_text = input(">")
+            qa_pairs = extract_qa_pairs(tokenizer, qg, summarizer, input_text)
+            print_qa_pairs(qa_pairs)
